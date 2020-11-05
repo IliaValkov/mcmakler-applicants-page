805e78df864b10d4629b0e4472b2d1a892856873
IliaValkov <ilia.valkov97@gmail.com>
2020-11-05 14:52:15 +0100

    Implemented page design mobile first

--- src/code/components/BaseLayout.tsx
+++ src/code/components/BaseLayout.tsx
@@ -1,5 +1,6 @@
+import "../../css/applicants-page.css";
 import React from "react";
-import {createState} from "../state"
+import {createState} from "../state";
 
 export default class BaseLayout extends React.Component {
     
@@ -16,17 +17,19 @@ export default class BaseLayout extends React.Component {
                     Back Headline Statistics
                     Search BidsFilter Status
                     ApplicantList1
-                        ApplicantCard1 ApplicantCard2 ... ApplicantCardN
+                        ApplicantCard1 ApplicantCard2 ...
+                    ... ApplicantCardN
                     ApplicantList2
-                        ApplicantCard1 ApplicantCard2 ... ApplicantCardN
+                        ApplicantCard1 ApplicantCard2 ...
+                    ... ApplicantCardN
             */
-            <div className="applicants-page">
-                <div>
+            <div className="applicantsPage">
+                <div className="information">
                     <span>
                         <img src="../src/media/icons/arrow-back.svg" alt="arrow-back-icon"/>
                     </span>
                     <h1>Applicants</h1>
-                    <div className="applicants-stats">
+                    <div className="applicantsStats">
                         <span>
                             <p> 25</p>
                             <p>Total</p>
@@ -49,22 +52,70 @@ export default class BaseLayout extends React.Component {
                         </span>
                     </div>
                 </div>
-                <div>
-                    <input type="text" className="search-box"/>
-                    <select name="bids-filter" placeholder="Bids"/>
-                    <select name="status-filter" placeholder="Status"/>
+                <div className="toolbar" >
+                    <span className="searchBox">
+                        <img src="../src/media/icons/search.svg" alt="search-icon" width="18px" height="18px"/>
+                        <input type="text" className="search-box" placeholder="Search for applicant"/>
+                    </span>
+                    <select className="filter" name="bidsFilter" id="bidsFilter">
+                        <option selected disabled value="None">Bids</option>
+                    </select>
+                    <select className="filter" name="statusFilter" id="statusFilter">
+                        <option selected disabled value="None">Status</option>
+                    </select>
                 </div>
-                <div className="applicant-lists">
+                <div className="applicantLists">
                     <section>
-                        <h2>Appointment set (1)</h2>
+                        <h2>Appointment set (2)</h2>
                         <ul>
                             <li>
-                                <p>I.V.</p>
-                                <p>Ilia Valkov</p>
-                                <p>+49 146 344 23811</p>
+                                <p className="initials">IV</p>
+                                <p className="fullName">Ilia Valkov</p>
+                                <p className="phoneNumber"> +49 146 344 23811</p>
+                                <p className="email">ilia@sample.com</p>
+                                <p className="statusAndDate">Appointment 22 July 19:00</p>
+                                <p className="bid">Bid 250 000 &euro;</p>
+                            </li>
+                            <li>
+                                <p className="initials">IV</p>
+                                <p className="fullName">Ilia Valkov</p>
+                                <p> +49 146 344 23811</p>
                                 <p>ilia@sample.com</p>
-                                <p>Appointment 22 July 19:00</p>
-                                <p>Bid 250 000 &euro;</p>
+                                <p className="statusAndDate">Appointment 22 July 19:00</p>
+                                <p className="bid">Bid 250 000 &euro;</p>
+                            </li>
+                        </ul>
+                    </section>
+                    <section>
+                        <h2>Interested (4)</h2>
+                        <ul>
+                            <li>
+                                <p className="initials">IV</p>
+                                <p className="fullName">Ilia Valkov</p>
+                                <p className="phoneNumber"> +49 146 344 23811</p>
+                                <p className="email">ilia@sample.com</p>
+                                <p className="statusAndDate">Interested</p>
+                            </li>
+                            <li>
+                                <p className="initials">IV</p>
+                                <p className="fullName">Ilia Valkov</p>
+                                <p> +49 146 344 23811</p>
+                                <p>ilia@sample.com</p>
+                                <p className="statusAndDate">Interested</p>
+                            </li>
+                            <li>
+                                <p className="initials">IV</p>
+                                <p className="fullName">Ilia Valkov</p>
+                                <p className="phoneNumber"> +49 146 344 23811</p>
+                                <p className="email">ilia@sample.com</p>
+                                <p className="statusAndDate">Interested</p>
+                            </li>
+                            <li>
+                                <p className="initials">IV</p>
+                                <p className="fullName">Ilia Valkov</p>
+                                <p> +49 146 344 23811</p>
+                                <p>ilia@sample.com</p>
+                                <p className="statusAndDate">Interested</p>
                             </li>
                         </ul>
                     </section>
