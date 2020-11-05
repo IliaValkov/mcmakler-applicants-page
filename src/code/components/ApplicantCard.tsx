6725b5e39390f5c160f2c0885b49bec963d08d8e
IliaValkov <ilia.valkov97@gmail.com>
2020-11-05 22:01:41 +0100

    Fixed card changing colors upon every state change bug

--- src/code/components/ApplicantCard.tsx
+++ src/code/components/ApplicantCard.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IApplicant, StatusType } from "../mock_api";
 
 type Props = {
-    pallete: {color: string, backgroundColor: string};
     applicant: IApplicant;
 }
 
@@ -13,13 +12,34 @@ type Props = {
  * colors for the initials element of the card
  * @param applicant An IApplicant object
  */
-export const ApplicantCard: React.FC<Props> = ({pallete, applicant}) => {
+export const ApplicantCard: React.FC<Props> = ({ applicant}) => {
+    type ColorPallete = {
+        color: string, backgroundColor: string
+    }
+
+    /* In order to keep the intially generated color, useState is used.
+    This way the component will remeber its pallete upon re-render
+    */
+    const [colorPallete, setColorPallete] = useState<ColorPallete>(generatePallete()); 
+    
+    /**
+     * Randomly picks a color from the preset color pallete
+     * 
+     * @returns [Object] The returned object has to keys, corresponding to the
+     * text and background colors to be used in an Applicant card
+     */
+    function generatePallete(): { color: string, backgroundColor: string } {
+        const bkgColorPallete = ["#FEE8D3", "#E3E7F9", "#CEF0F4", "#F0F4E1", "#FCD6DE"];
+        const textColorPallete = ["#E97428", "#405493", "#20ADC5", "#ACAF36", "#E52A50"];
+        const randomNumber = Math.floor(Math.random() * 4);
+        return { color: textColorPallete[randomNumber], backgroundColor: bkgColorPallete[randomNumber] };
+    }
     
     // Extract the initials of the name and declare the style object
     const initials = applicant.firstName.charAt(0).toUpperCase() + applicant.lastName.charAt(0).toUpperCase();
     const initialsStyle: React.CSSProperties = {
-        backgroundColor: pallete.backgroundColor,
-        color: pallete.color  
+        backgroundColor: colorPallete.backgroundColor,
+        color: colorPallete.color  
     };
 
     // Determine the status string based on the status property of the received applicant object 
--- src/code/components/ApplicantList.tsx
+++ src/code/components/ApplicantList.tsx
@@ -15,18 +15,7 @@ type Props = {
  */
 export const ApplicantList: React.FC<Props> = ({ name, list }) => {
 
-    /**
-     * Randomly picks a color from the preset color pallete
-     * 
-     * @returns [Object] The returned object has to keys, corresponding to the
-     * text and background colors to be used in an Applicant card
-     */
-    function generatePallete(): { color: string, backgroundColor: string } {
-        const bkgColorPallete = ["#FEE8D3", "#E3E7F9", "#CEF0F4", "#F0F4E1", "#FCD6DE"];
-        const textColorPallete = ["#E97428", "#405493", "#20ADC5", "#ACAF36", "#E52A50"];
-        const randomNumber = Math.floor(Math.random() * 4);
-        return { color: textColorPallete[randomNumber], backgroundColor: bkgColorPallete[randomNumber] };
-    }
+    
 
     return (
         <section>
@@ -34,7 +23,6 @@ export const ApplicantList: React.FC<Props> = ({ name, list }) => {
             <ul>
                 {list.map(applicant =>
                     <ApplicantCard
-                        pallete={generatePallete()}
                         applicant={applicant} />)
                 }
             </ul>
